9655dc0a29fa403a07c266052e65a14e3a5ec477
Romadillo <84402330+Romadillo@users.noreply.github.com>
2022-11-01 10:39:59 -0400

    Uploaded SampleFocus Downloader extension

---
+++ SampleFocus Downloader.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         SampleFocus Downloader
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Download unlimited samples from SampleFocus
+// @author       Romadillo
+// @match        https://samplefocus.com/samples/*
+// @icon         https://i.imgur.com/WedmtXe.png
+// @grant        GM_download
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const element = document.getElementsByClassName("sample-hero-waveform-container");
+    const sampleUrl = JSON.parse(element[0].attributes[1].value)["sample"]["sample_mp3_url"];
+
+    const link = document.querySelector(".sample-hero .download-link");
+    link.onclick = function(e) {
+        const fileName = new URL(sampleUrl).pathname.split("/").pop();
+        console.log("Downloading ", sampleUrl, " as ", fileName);
+        GM_download({url: sampleUrl, name: fileName, saveAs: true, onerror: function(e) { console.log(e); }});
+        e.stopPropagation();
+        return false;
+    }
+})();
